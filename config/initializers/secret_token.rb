54137549eeb490f4b90b645fa202f75101586a86
Edijs Petersons <eddpeterson@gmail.com>
2011-07-25 14:00:22 +0300

    Created project structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheBucketList::Application.config.secret_token = '1a71d56ec93ff9f15355478260a50f796e6c24d01e989014f47efccb0e01551d167a06f3462e9cd8ca1c44d4adeff552cb5ff30d2b36eafedb835689f3c1c9a8'
