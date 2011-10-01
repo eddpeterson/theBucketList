6b9d3559dcfdd98b2d6b457514877179f8c323e8
Edijs Petersons <eddpeterson@gmail.com>
2011-10-01 19:31:23 +0200

    Updated root url

--- config/routes.rb
+++ config/routes.rb
@@ -52,8 +52,8 @@ TheBucketList::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'todos#new'
-
+  root :to => 'todos#index' 
+  
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
