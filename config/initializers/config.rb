38b416c91fa8ce7917f849a4245397a361616711
Edijs Petersons <eddpeterson@gmail.com>
2011-11-07 21:25:08 +0100

    Fixiing Production ENV variable null reference error

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,3 +1,8 @@
-if Rails.env.production? == false
-  APP_CONFIG = YAML.load_file(Rails.root.join('config', 'config.yml'))[ENV["RAILS_ENV"]]
+if Rails.env.production?
+  FACEBOOK_APP_ID = ENV['facebook_app_id']
+  FACEBOOK_APP_SECRET = ENV['facebook_app_secret'] 
+else
+  app_config = YAML.load_file(Rails.root.join('config', 'config.yml'))[ENV["RAILS_ENV"]]
+  FACEBOOK_APP_ID = app_config['facebook_app_id']
+  FACEBOOK_APP_SECRET = app_config['facebook_app_secret']
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,13 +8,10 @@ Devise.setup do |config|
   
   # #config.omniauth :facebook, "APP_ID", "APP_SECRET"
   # # https://github.com/plataformatec/devise/wiki/OmniAuth:-Overview
-  if Rails.env.production?
-    config.omniauth :facebook, ENV['facebook_app_id'], ENV['facebook_app_secret'], 
-      {:scope => 'offline_access,email,user_birthday'}
-  else
-    config.omniauth :facebook, APP_CONFIG['facebook_app_id'], APP_CONFIG['facebook_app_secret'], 
-      {:scope => 'offline_access,email,user_birthday', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
-  end
+  puts "................... id "
+  puts FACEBOOK_APP_ID
+  config.omniauth :facebook, FACEBOOK_APP_ID, FACEBOOK_APP_SECRET, 
+    {:scope => 'offline_access,email,user_birthday', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
   # see full list of permissions here: http://developers.facebook.com/docs/reference/api/user/
   
   # # If you run into an OpenSSL error like this:
